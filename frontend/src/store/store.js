4394ca58c44dc57aeaabd82bc663933631db16d3
hiarun01 <hiarun02@gmail.com>
2025-06-06 11:09:45 +0530

    add empty chat container

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,8 @@
 import {create} from "zustand";
 import {authSlice} from "./authSlice/authSlice";
+import {chatSlice} from "./authSlice/chatSlice";
 
 export const useAppStore = create((...a) => ({
   ...authSlice(...a),
+  ...chatSlice(...a),
 }));
